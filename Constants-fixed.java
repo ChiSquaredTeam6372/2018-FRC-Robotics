3c7f5f32487b28d3eed4378598fed75d243a9872
Kirsten Naughton <kirsten.naughton@gmail.com>
2018-02-14 06:47:52 -0700

    fixed comment

--- Constants-fixed.java
+++ Constants-fixed.java
@@ -1,4 +1,4 @@
-Constants.java               fixed program working file 3
+//Constants.java               fixed program working file 3
 
 package org.usfirst.frc.team6372.robot;
 
@@ -15,14 +15,13 @@ public class Constants {
   public static final int TALON_RL_PORT = 5;
   public static final int TALON_RR_PORT = 6;
 
-
   public static final int JOYSTICK_PORT = 0;
   public static final int AJOYSTICK_PORT = 1;
   public static final double MAX_DRIVE_SPEED = .5;
   public static final double MAX_TURN_SPEED = .35;
   public static final double DRIVE_SPEEDR = .25;
   public static final double DRIVE_SPEEDL = .25;
-  
+
   public static final double MAX_DRIVE_SPEED_ROLLERL = .05;
   public static final double MAX_DRIVE_SPEED_ROLLERR = .05;
   public static final double DRIVE_SPEED_ROLLERL = .05;
@@ -33,11 +32,10 @@ public class Constants {
   public static final int JOYSTICK_LEFT_DRIVE_AXIS = 1;
   public static final int JOYSTICK_RIGHT_DRIVE_AXIS = 1;
   public static final int JOYSTICK_TURN_DRIVE_AXIS = 4;
-  
+
   public static final int JOYSTICK_ROLLERL_DRIVE_AXIS = 3;
   public static final int JOYSTICK_ROLLERR_DRIVE_AXIS = 3;
   public static final int JOYSTICK_EXTEND_DRIVE_AXIS = 5;
-  
 
   public static final int CAMERA_SERVER = 1;
   public static final int RESOLUTION = 360;
