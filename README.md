f35c504c44918bb63d58a5628ac0479ee35b56d7
Kirsten Naughton <kirsten.naughton@gmail.com>
2018-02-19 10:08:45 -0700

    differential drive first attempt at organizing, robotdrive is deprecated

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 2018-FRC-Robotics
+
 File versioning and working code snippet storage for FRC Team 6372 with programmers Will and Ethan :)
+
+Copies of all test files so they have lots of info for comparison.
---
+++ Robot-possibleDifferential.java
@@ -0,0 +1,204 @@
+package org.usfirst.frc.team6372.robot;
+
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Timer;
+
+//@SuppressWarnings("deprecation")
+public class Robot extends IterativeRobot {
+  public static final Subsystem exampleSubsystem = null;
+  DriveTrain driveTrain;
+  //DriverStation getGameSpecificMessage;
+  DriverStation driverStation;
+  DifferentialDrive differentialDrive;
+  XboxController joystick;
+  Joystick joystickx;
+  Timer timer = new Timer();
+  //DifferentialDrive drive = new differentialDrive(1,2,3,4);
+
+  public class DifferentialDrive extends IterativeRobot {
+    DifferentialDrive drive = new DifferentialDrive();
+  }
+
+  @Override
+  public void robotInit() {
+    driveTrain = new DriveTrain();
+    joystick = new XboxController(Constants.JOYSTICK_PORT);
+    joystickx = new Joystick(Constants.AJOYSTICK_PORT);
+    //driverStation = new DriverStation();
+    CameraServer.getInstance().startAutomaticCapture();
+
+  }
+
+  @Override
+  public void autonomousInit() {
+    timer.reset();
+    timer.start();
+
+    /*	String gameData;
+    	 gameData = DriverStation.getInstance().getGameSpecificMessage();
+    	 	if(gameData.length() > 0) {
+    		 
+    		 if(gameData.charAt(0) == 'L')
+    		 {
+    */
+
+    /*		 if (timer.get() < 1.5) {
+    			driveTrain.drive(-0.53, -0.57);
+    		}
+    
+    		 else if (timer.get() == 1.5) {
+    				driveTrain.drive(0.0, 0.0, 0);
+    			}
+    
+    		 else if (timer.get() > 2.2 && timer.get() < 3.5)
+    			{driveTrain.drive(0.57, 0.61);}
+    		else if (timer.get() > 6.6 && timer.get() < 7.5) {
+    			driveTrain.drive(-0.57, 0.61);}
+    		else if (timer.get() > 7.6 && timer.get() < 12.65) {
+    			driveTrain.drive(0.57, 0.61);
+    		}
+    		else if (timer.get() > 12.8 && timer.get() < 13.7) {
+    				 
+    			driveTrain.drive(0.57, 0.61);}
+    		else {
+    			driveTrain.drive(0.0, 0.0);
+    		}
+    
+    		 
+    	 }else {
+     
+    		  
+    		 	if (timer.get() < 1.5)	{
+    			driveTrain.drive(0.53, 0.57);
+    		}
+    
+    		else if (timer.get() == 1.5) {
+    			driveTrain.drive(0.0, 0.0, 0);
+    		}
+    
+    		 	else if (timer.get() > 1.75 && timer.get() < 2.6) {
+    				driveTrain.drive(-0.57, -0.61);
+    			}
+    		else if (timer.get() > 2.8 && timer.get() < 6.5)
+    			{driveTrain.drive(-0.57, -0.61);}
+    		else if (timer.get() > 6.7 && timer.get() < 7.71) {
+    			driveTrain.drive(-0.57, 0.61);}
+    		else if (timer.get() > 7.9 && timer.get() < 12.95) {
+    			driveTrain.drive(-0.57, -0.61);
+    		}
+    		else if (timer.get() > 13.1 && timer.get() < 14.1
+    				) {
+    			driveTrain.drive(-0.57, 0.61);}
+    		else {
+    			driveTrain.drive(0.0, 0.0);
+    	
+    		} */
+    //	 } 
+    //	}  	
+
+  }
+
+  //Robot.java    Fixed Autonomous - More control on timer
+  //backwards and forwards on the if/else statement, 
+  //autonomous timer with a stop in the middle
+  @Override
+  public void autonomousPeriodic() {
+
+    String gameData;
+    gameData = DriverStation.getInstance().getGameSpecificMessage();
+    if (gameData.length() > 0) {
+
+      if (gameData.charAt(0) == 'L') {
+
+        if (timer.get() < 1.5) {
+          driveTrain.drive(-0.57, -0.61, 0); // drive forwards half speed
+        }
+
+        else if (timer.get() == 1.5) {
+          driveTrain.drive(0.0, 0.0, 0);
+        }
+
+        else if (timer.get() > 1.75 && timer.get() < 2.5) {
+          driveTrain.drive(0.57, -0.61, 0);
+        } else if (timer.get() > 2.8 && timer.get() < 6.5) {
+          driveTrain.drive(-0.57, -0.61, 0);
+        } else if (timer.get() > 6.6 && timer.get() < 7.3) {
+          driveTrain.drive(-0.57, 0.61, 0);
+        } else if (timer.get() > 7.6 && timer.get() < 12.45) {
+          driveTrain.drive(-0.57, -0.61, 0);
+        } else if (timer.get() > 12.8 && timer.get() < 13.55) {
+          driveTrain.drive(-0.57, 0.61, 0);
+        } else {
+          driveTrain.drive(0.0, 0.0, 0);
+        }
+
+      } else {
+
+        if (timer.get() < 1.5) {
+          driveTrain.drive(-0.53, -0.57, 0);
+        }
+
+        else if (timer.get() == 1.5) {
+          driveTrain.drive(0.0, 0.0, 0);
+        }
+
+        else if (timer.get() > 1.75 && timer.get() < 2.5) {
+          driveTrain.drive(-0.57, 0.61, 0);
+        } else if (timer.get() > 2.8 && timer.get() < 6.5) {
+          driveTrain.drive(-0.57, -0.61, 0);
+        } else if (timer.get() > 6.6 && timer.get() < 7.3) {
+          driveTrain.drive(0.57, -0.61, 0);
+        } else if (timer.get() > 7.6 && timer.get() < 12.45) {
+          driveTrain.drive(-0.57, -0.61, 0);
+        } else if (timer.get() > 12.8 && timer.get() < 13.55) {
+          driveTrain.drive(0.57, -0.61, 0);
+        } else {
+          driveTrain.drive(0.0, 0.0, 0);
+        }
+
+      }
+    }
+  }
+
+  @Override
+  public void teleopInit() {
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+
+    driveTrain.drive(joystick.getRawAxis(Constants.JOYSTICK_LEFT_DRIVE_AXIS),
+        joystick.getRawAxis(Constants.JOYSTICK_RIGHT_DRIVE_AXIS),
+        joystick.getRawAxis(Constants.JOYSTICK_TURN_DRIVE_AXIS));
+
+    driveTrain.drive1(joystick.getRawAxis(Constants.JOYSTICK_ROLLERL_DRIVE_AXIS),
+        joystick.getRawAxis(Constants.JOYSTICK_ROLLERR_DRIVE_AXIS),
+        joystick.getRawAxis(Constants.JOYSTICK_EXTEND_DRIVE_AXIS));
+
+    driveTrain.drive2(joystick.getRawAxis(Constants.JOYSTICK_ROLLERL1_DRIVE_AXIS),
+        joystick.getRawAxis(Constants.JOYSTICK_ROLLERR1_DRIVE_AXIS),
+        joystick.getRawAxis(Constants.JOYSTICK_EXTEND1_DRIVE_AXIS));
+
+    driveTrain.drive3(joystick.getRawAxis(Constants.JOYSTICK_CLIMBERL_DRIVE_AXIS),
+        joystick.getRawAxis(Constants.JOYSTICK_CLIMBERR_DRIVE_AXIS));
+
+  }
+
+  @Override
+  public void testInit() {
+
+  }
+
+  @Override
+  public void testPeriodic() {
+
+  }
+}
